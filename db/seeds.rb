a2a9ac98dc99aa13fd46ee2ee30e72e498d83f25
Pieter <gistelinckpieter@gmail.com>
2016-08-23 16:51:47 +0200

    second seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
 require 'date'
 
-customer1 = Customer.create!(email: "customer1@gmail.com", first_name: "Paul", last_name: "Gistelinck", phone_number: "+32867565675", mobile_number: "+337687678676", password: "123456")
-customer2 = Customer.create!(email: "customer2@gmail.com", first_name: "Sandrine", last_name: "Bontinckx", phone_number: "+32867565675", mobile_number: "+337687678676", password: "123456")
+customer1 = Customer.create!(
+
+  email: "customer1@gmail.com",
+  first_name: "Paul",
+  last_name: "Gistelinck",
+  address: "Karmstraat 19, 9790 Ooike",
+  birthday: Date.new(1990,8,20),
+  phone_number: "+32867565675",
+  mobile_number: "+337687678676",
+  password: "123456",
+  signing_date: Date.new(2016,8,20))
+
+customer2 = Customer.create!(
+
+  email: "customer2@gmail.com",
+  first_name: "Sandrine",
+  last_name: "Bontinckx",
+  address: "Emilius Seghersplein 19, 9000 Gent",
+  birthday: Date.new(1990,8,20),
+  phone_number: "+32867565675",
+  mobile_number: "+337687678676",
+  password: "123456",
+  signing_date: Date.new(2016,8,20))
 
 
 

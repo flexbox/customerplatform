29382efea29136aedd5576d9e0812cd1c3536fe3
Pieter <gistelinckpieter@gmail.com>
2016-08-24 14:59:45 +0200

    link

--- app/controllers/customers/dashboards_controller.rb
+++ app/controllers/customers/dashboards_controller.rb
@@ -2,5 +2,8 @@ class Customers::DashboardsController < Customers::BaseController
   def show
     @unit = current_customer.units.first
     @units = current_customer.units
+
+
+    @units = current_customer.units
   end
 end

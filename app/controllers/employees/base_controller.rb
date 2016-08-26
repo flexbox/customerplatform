961a8a802e816b2af75ebd6fd1340736c36dda16
Marc Francis <marc.francis.mail@gmail.com>
2016-08-26 15:45:42 +0200

    Fix employee base and projects controllers

--- app/controllers/employees/base_controller.rb
+++ app/controllers/employees/base_controller.rb
@@ -9,32 +9,32 @@ class Employees::BaseController < ApplicationController
     def set_project
       # @project will be nil if the current employee is not attached to any project
       @project = nil
-      unless current_employee.projects.nil? || current_employee.projects.empty?
-        if params[:project_id]
-          projects = current_employee.projects
-          projects.each do |project|
-            if project.id == params[:project_id]
-              @project = project if @project.nil?
-            end
-          end
-        else
-          @project = current_employee.projects.first
-        end
-      end
+      # unless current_employee.projects.nil? || current_employee.projects.empty?
+      #   if params[:project_id]
+      #     projects = current_employee.projects
+      #     projects.each do |project|
+      #       if project.id == params[:project_id]
+      #         @project = project if @project.nil?
+      #       end
+      #     end
+      #   else
+      #     @project = current_employee.projects.first
+      #   end
+      # end
     end
 
   def set_unit_employee
     # @unit will be nil if there's no unit related to the current employee
     @unit = nil
-    unless current_employee.projects.nil? || current_employee.projects.empty?
-      if params[:unit_id]
-        un = Unit.find(params[:unit_id])
-        emp = un.building.lot.phase.project.employees.first
-        @unit = un if emp == current_employee
-      else
-        @unit = current_employee.projects.first.phases.first.lots.first.buildings.first.units.first
-      end
-    end
+    # unless current_employee.projects.nil? || current_employee.projects.empty?
+    #   if params[:unit_id]
+    #     un = Unit.find(params[:unit_id])
+    #     emp = un.building.lot.phase.project.employees.first
+    #     @unit = un if emp == current_employee
+    #   else
+    #     @unit = current_employee.projects.first.phases.first.lots.first.buildings.first.units.first
+    #   end
+    # end
   end
 
 end

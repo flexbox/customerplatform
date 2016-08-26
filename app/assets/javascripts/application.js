99dae6c5357cf3aaac5cfcbcac4974829a180479
Yannick Bontinckx <yannick@bontinckx.be>
2016-08-26 19:13:15 +0200

    Remove Material Design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
-//= require bootstrap-material-design
 //= require underscore
 //= require gmaps/google
+//= require_tree .
 
 
 

2cc0e45ba17c7a14395a8c3a9b468f199e4dde16
Pieter <gistelinckpieter@gmail.com>
2016-08-31 22:12:59 +0200

    blurred images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830151125) do
+ActiveRecord::Schema.define(version: 20160831200621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20160830151125) do
     t.string   "invited_by_type"
     t.integer  "invited_by_id"
     t.integer  "invitations_count",      default: 0
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["email"], name: "index_customers_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_customers_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_customers_on_invitations_count", using: :btree
